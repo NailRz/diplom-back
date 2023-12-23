b9a6c7e97c006c1ff294c51e08771bd2478c59cb
NailRz <nailrzr@gmail.com>
2023-12-23 04:24:06 +0300

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule)
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://127.0.0.1:5173', // Замените на ваш домен фронтенда
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
 
   await app.listen(PORT, () => console.log(`Serv started! on port  = ${PORT }`))
 }

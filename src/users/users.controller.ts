73db95e263f2509011f6f42168e76b6563ce76c6
Nail <fakenail@ya.ru>
2024-04-14 22:17:15 +0300

    getUserByEmail added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { createUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
@@ -8,16 +8,21 @@ export class UsersController {
     constructor(private usersService: UsersService){
 
     }
-
-
     @Post()
     create(@Body() userDto: createUserDto){
         return this.usersService.createUser(userDto);
     }
 
+
     @UseGuards(JwtAuthGuard)
     @Get()
     getAll(){
         return this.usersService.getAllUsers();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':email')
+    async getUserByEmail(@Param('email') email: string){
+        return this.usersService.getUsersByEmail(email);
     }
 }
